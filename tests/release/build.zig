c885f687a7086c032933627092cde0e622a3936c
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-08-20 16:57:54 +0200

    feat: add release test & release to AIO action

---
+++ tests/release/build.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe_mod = b.addModule("main", .{
+        .root_source_file = b.path("main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "main",
+        .root_module = exe_mod,
+    });
+
+    b.installArtifact(exe);
+}
